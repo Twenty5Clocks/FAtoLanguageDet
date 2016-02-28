8598dcb5adc5b24b5623d2bb48408a841ca4fe6f
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-02-28 13:55:58 -0800

    added example solution

--- FAtoLanguage/FAtoLanguage/Prob3.cpp
+++ FAtoLanguage/FAtoLanguage/Prob3.cpp
@@ -9,14 +9,42 @@
 // This program displays:
 //       Evaluate postfix expressions with multiple letter variables
 //------------------------------------------------------------------------------------------
+/*
+Given L write a program to determine whether the word w is accepted or rejected by L.
+L = a*b*+c*
+Create a complete FA then find table
+a	b	c
+0	3	4	1
+1	2	2	1
+2	2	2	2
+3	3	4	2
+4	2	4	2
+*/
 #include <iostream>
-
+#include <string>
 using namespace std;
-int main()
-{
-	cout << "This is working!!!" << endl;
-	cout << "New Line." << endl;
-	cout << "adding yet another line" << endl;
+
+int main() {
+	//Array containg table:
+	int Table[5][3] = { { 3,4,1 },{ 2,2,1 },{ 2,2,2 },{ 3,4,2 },{ 2,4,2 } };
+
+	string w = "aabbb";
+	int i = 0, col, state = 0;
+	while (i < w.length()) {
+		switch (w[i]) {
+		case 'a':
+			col = 0; break;
+		case 'b':
+			col = 1; break;
+		case 'c':
+			col = 2; break;
+		}
+		state = Table[state][col];
+
+		i++;
+	}
+	if (state == 2) cout << w << "is rejected";
+	else cout << w << "is accepted";
 	system("pause");
 	return 0;
 }
